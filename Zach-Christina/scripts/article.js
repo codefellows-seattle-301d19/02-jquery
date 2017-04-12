5373ef3a656fef407d6c891061e70a0629bcc407
Zachary Diehl <zachary.diehl@gmail.com>
2017-04-12 11:02:48 -0700

    finished all TODOS and data is correctly populating to page

--- Zach-Christina/scripts/article.js
+++ Zach-Christina/scripts/article.js
@@ -3,39 +3,25 @@
 var articles = [];
 
 function Article (rawDataObj) {
-  // : Use the JS object passed in to complete this constructor function:
   this.title = rawDataObj.title;
   this.category = rawDataObj.category;
   this.author = rawDataObj.author;
   this.authorUrl = rawDataObj.authorUrl;
   this.publishedOn = rawDataObj.publishedOn;
   this.body = rawDataObj.body;
-  // articles.push(this);
 }
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.removeClass('template');
-  /* TODO: This cloned article still has a class of template.
-  However, in our modules.css stylesheet, we gave all elements
-  with a class of template a display of none. Let's make
-  sure we're not accidentally hiding our cloned article! */
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.data('category', this.category);
 
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest
-  of the current template clone with properties from this particular Article instance.
-  We need to fill in:
-    1. author name,
-    2. author url,
-    3. article title,
-    4. article body, and
-    5. publication date. */
   $newArticle.find('h1').html(this.title);
   $newArticle.find('a').html(this.author);
   $newArticle.find('a').attr('href', this.authorUrl);
   $newArticle.find('time').html(this.publishedOn);
-  $newArticle.find('#article-body').html(this.body);
+  $newArticle.find('section').html(this.body);
   // Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   $newArticle.append('<hr>');
